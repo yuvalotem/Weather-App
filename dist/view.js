0a33d2b6f2ff5b0d894e36c9c58ed1f6d46b5b8c
yuvalotem <yuvalotem@gmail.com>
2020-09-10 17:51:05 +0300

    new changes

--- dist/view.js
+++ dist/view.js
@@ -2,9 +2,10 @@ class Render {
     constructor(){}
 
     renderData(data){
+        console.log(data);
         const source = $('#weather-template').html()
         const template = Handlebars.compile(source)
-        const newHTML = template({ data })
-        $('#weather').empty().append(newHTML)
+        const newHTML = template(data[0])
+        $('#city-container').empty().append(newHTML)
     }
 }
